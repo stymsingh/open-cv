b0bff4a2fa9613435aac0101cd583639969a018e
satyam singh <anonymoussatyam1997@hotmail.com>
2017-02-18 13:31:22 +0530

    second commit

--- reading an image.py
+++ reading an image.py
@@ -2,6 +2,8 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
+# open cv code
+
 img = cv2.imread('index.jpg',0)
 cv2.imshow('image',img)
 k = cv2.waitKey(0)#wait for any key to be pressed
